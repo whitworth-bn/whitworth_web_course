ee6185798d70dc87b59fde4ed4a2a532c3de7e7f
Tab-160 <rab.greenup@gmail.com>
2025-03-19 18:05:08 -0700

    Added Rab's Project 3

--- WEB1-Student-Sites/greenup_r/Project_3/js/index.js
+++ WEB1-Student-Sites/greenup_r/Project_3/js/index.js
@@ -1,3 +1,32 @@
-import '@material/web/button/filled-button.js';
-import '@material/web/button/outlined-button.js';
-import '@material/web/checkbox/checkbox.js';
+function change_color(){
+	var colorInput = document.getElementById('color-input');
+	var selectedColor = colorInput.value;
+	set_colors(selectedColor);
+}
+
+
+
+// Get the modal
+var color_input = document.getElementById("color_input_div");
+var color_input_shadow = document.getElementById("color_input_shadow");
+// Get the button that opens the modal
+var palette = document.getElementById("palette");
+// Get the  element that closes the modal
+var close = document.getElementById("close_color_input");
+// When the user clicks the button, open the modal 
+palette.onclick = function() {
+	color_input.style.display = "flex";
+	color_input_shadow.style.display = "flex";
+}
+// When the user clicks on  (x), close the modal
+close.onclick = function() {
+	color_input.style.display = "none";
+	color_input_shadow.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target != palette && event.target != document.getElementById('color-input')) {
+		color_input.style.display = "none";
+		color_input_shadow.style.display = "none";
+	}
+}
