28fbd33e5184b2967feb6bbb88874412774baaa4
sbyrne03 <123109247+sbyrne03@users.noreply.github.com>
2025-05-13 11:56:25 -0700

    Update main.js

--- WEB1-Student-Sites/byrne_s/Project_5_Final/static/main.js
+++ WEB1-Student-Sites/byrne_s/Project_5_Final/static/main.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const frameIndex = Math.min(frameCount - 1, Math.floor(scrollFraction * frameCount)) + 1;
 
         if (frameIndex !== currentFrame) {
-            nextImage.src = `static/images/frames/wave${frameIndex}.svg`;
+            nextImage.src = `images/frames/wave${frameIndex}.svg`;
             nextImage.classList.remove("fade");
 
             setTimeout(() => {
@@ -39,25 +39,25 @@ fetch('static/images/homepage.html')
                 const name = el.getAttribute('id');
                 // Redirect based on the section name
                 if (name === 'Generative_Object') {
-                    window.location.href = '/mountain.html';
+                    window.location.href = '../mountain.html';
                 }
                 else if (name === 'Generative_Object-2') {
-                    window.location.href = '/grassland.html';
+                    window.location.href = '../grassland.html';
                 }
                 else if (name === 'Generative_Object-3') {
-                    window.location.href = '/arctic.html';
+                    window.location.href = '../arctic.html';
                 }
                 else if (name === 'Generative_Object-4') {
-                    window.location.href = '/forest.html';
+                    window.location.href = '../forest.html';
                 }
                 else if (name === 'Generative_Object-5') {
-                    window.location.href = '/ocean.html';
+                    window.location.href = '../ocean.html';
                 }
                 else if (name === 'Generative_Object-6') {
-                    window.location.href = '/desert.html';
+                    window.location.href = '../desert.html';
                 }
                 else if (name === 'Generative_Object-7') {
-                    window.location.href = '/sky.html';
+                    window.location.href = '../sky.html';
                 }
                 // Add more cases as needed
             });
